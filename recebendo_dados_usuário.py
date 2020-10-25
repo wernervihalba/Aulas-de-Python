a3afe2d6e698a94f1cd50be731e7bd17e259911c
wernervihalba <wernervihalba@users.noreply.github.com>
2020-10-25 16:02:43 -0300

    Create recebendo_dados_usuário.py

---
+++ recebendo_dados_usuário.py
@@ -0,0 +1,22 @@
+"""
+Recebendo dados do usuário
+
+"""
+# Entrada de dados
+print('Qual o seu nome?')
+nome = input() # input significa entrada de dado.
+
+# Exemplo de print antigo - versão 2.0 do Python
+print('Seja Bem-vindo(a) %s' %nome)
+
+print('Qual a sua idade?')
+idade = input()
+
+#Processamento
+
+#Saída de dados
+
+#Saída de dados antigo - Python 2.0
+
+print(' %s tem %s anos' % (nome,idade))
+
