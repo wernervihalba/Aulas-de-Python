8fd27e9c1f7b18395b9d12701e9a84887266d297
wernervihalba <wernervihalba@users.noreply.github.com>
2020-10-25 16:28:53 -0300

    Update recebendo_dados_usuário.py

--- recebendo_dados_usuário.py
+++ recebendo_dados_usuário.py
@@ -1,7 +1,7 @@
 """
-Recebendo dados do usuário
+- Recebendo dados do usuário
 
-"""
+
 # Entrada de dados
 print('Qual o seu nome?')
 nome = input() # input significa entrada de dado.
@@ -20,3 +20,21 @@ idade = input()
 
 print(' %s tem %s anos' % (nome,idade))
 
+
+
+"""
+
+#Exemplo de print moderno - apartir do 3.0
+# print('Seja bem vindo(a) {0} .format (nome))
+
+#Exemplo de Python mais moderno - Depois da versão 3.7
+
+print('Qual o seu nome?')
+nome = input()
+
+print('Qual a sua idade?')
+idade = input()
+
+print(f' {nome} tem {idade} anos')
+
+print(f' E nasceu em {2020 - int(idade)}')
