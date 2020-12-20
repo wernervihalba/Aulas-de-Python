646d928bc31688dbd08962d62905e32cb20c5713
wernervihalba <wernervihalba@users.noreply.github.com>
2020-12-20 20:24:39 -0300

    Variáveis
    Aprender o básico das váriaveis

--- Tipo String.py
+++ Tipo String.py
@@ -2,7 +2,27 @@
 Tipo String
 em Python, um dado é considerado string quando:
 
-Estiver em aspas simples ' ' -> 'abc', '123'
+Estiver em aspas simples ou duplas  -> 'abc', '123' - "abc", "123"
+nome = "Spoky"
+print(nome)
+print(type(nome))
 
-
+nome = 'Angelina \nJolie'
+print(nome)
+print(type(nome))
 """
+
+nome = "Spoky"
+print(nome)
+print(type(nome))
+
+nome = 'Angelina \nJolie'
+print(nome)
+print(type(nome))
+
+nome = 'Thawanna'
+print(nome.upper())
+
+print(nome.lower())
+
+print(nome.split())
