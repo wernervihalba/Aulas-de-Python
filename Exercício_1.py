347d54601562a79de940b8a4d2483709adc4e237
wernervihalba <wernervihalba@users.noreply.github.com>
2020-12-20 21:27:44 -0300

    Exercicios _4 sessões
    Lista de exercícios das últimas sessões .

---
+++ Exercício_1.py
@@ -0,0 +1,12 @@
+"""
+Exercício 1
+Leia uma temperatura em graus Celsius, e apresente convertida em graus Fahrenheit.
+A formula de conversão é: F=c*(9.0/5.0)+30.2, sendo f a temperatura em Fahrenheit e C em Temperatura em Celcius
+
+Exercício 2
+
+Faça agora o processo invertido, leia a temperatura em Fahrenheit e apresente em Celcius.
+A formula de conversão é C=5.0*(F-32.0)/9.0, sendo C para Celcius e F para Fahrenheit.
+
+"""
+
